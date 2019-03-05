21c340767646d09011891efa48fedaa7ef94ad2f
Sophie Nichol <snichol00@stuy.edu>
2019-03-05 10:12:14 -0500

    started to read in text file

--- USACO.java
+++ USACO.java
@@ -1,6 +1,10 @@
 public class USACO{
   public static int bronze(String filename){
-
+    File text = new File("file.txt");
+    Scanner inf = new Scanner(text);
+    while(inf.hasNextLine()){
+          String line = inf.nextLine();
+          System.out.println(line);
   }
 
   public static int silver(String filename){
