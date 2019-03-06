fc74aef40169c0d5db59ee442871df4b166d82c6
Sophie Nichol <snichol00@stuy.edu>
2019-03-06 10:12:30 -0500

    added cowStomp

--- USACO.java
+++ USACO.java
@@ -9,7 +9,7 @@ public class USACO{
           lines.add(line);
     }
 
-    int[] params = lines.get(0).toCharArray();
+    int[] params = lines.get(0).split(" ");
     int r = params[0];
     int c = params[1];
     int e = params[2];
@@ -21,13 +21,19 @@ public class USACO{
     }
 
     for (int idx = r + 1; idx < ArrayList.size(); idx ++){
-      String[] stomp = lines.get(idx).toCharArray();
+      String[] stomp = lines.get(idx).split(" ");
       cowStomp(stomp[0], stomp[1], stomp[2]);
     }
 
     private void cowStop(int row, int col, int depth){
-      
+      for (; row < row + 3; row++){
+        for (; col < col + 3; col++){
+          land[row][col] -= depth;
+        }
+      }
     }
+
+    int output = 0;
   }
 
   public static int silver(String filename){
