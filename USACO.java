87ff39a7b1fb24387e5084972fc4ef24b46f385b
Sophie Nichol <snichol00@stuy.edu>
2019-03-05 22:11:55 -0500

    outlined some basic functions

--- USACO.java
+++ USACO.java
@@ -2,9 +2,24 @@ public class USACO{
   public static int bronze(String filename){
     File text = new File("file.txt");
     Scanner inf = new Scanner(text);
+    ArrayList<String> lines = new ArrayList<String>();
     while(inf.hasNextLine()){
           String line = inf.nextLine();
-          System.out.println(line);
+          lines.add(line);
+    }
+    int r;
+    int c;
+    int n;
+    int e;
+
+    for (int idx = r + 1; idx < ArrayList.size(); idx ++){
+      input = ArrayList.get(idx);
+      cowStomp(input);
+    }
+
+    private void cowStop(int r, int c, int depth){
+      
+    }
   }
 
   public static int silver(String filename){
