5bee7228bac90e546b37d0288c3e21fa429ad1fe
Sophie Nichol <snichol00@stuy.edu>
2019-03-07 09:59:54 -0500

    added comments and tested more

--- USACO.java
+++ USACO.java
@@ -4,15 +4,18 @@ import java.util.*;
 public class USACO{
   public static int bronze(String filename){
     try{
+      //read in file
       File text = new File(filename);
       Scanner inf = new Scanner(text);
       ArrayList<String> lines = new ArrayList<String>();
 
+      // make variables for the params
       int r = inf.nextInt();
       int c = inf.nextInt();
       int e = inf.nextInt();
       int n = inf.nextInt();
 
+      // makes an array for the lake values
       int[][] lake = new int[r][c];
       for (int row = 0; row < r; row++){
         for (int col = 0; col < c; col++){
@@ -20,21 +23,28 @@ public class USACO{
         }
       }
 
+      //makes an array of all the stomp commands
       int[][] stomps = new int[n][3];
       for (int row = 0; row < n; row++){
         for (int col = 0; col < 3; col++){
+          // each row contained the three params of stomp
           stomps[row][col] = inf.nextInt();
         }
       }
       //System.out.println(toString(lake));
-      //
+
+      //loops through stomp commands
       for (int row = 0; row < stomps.length; row++){
+        // keeps track of the highest stomp elevation
         int max = 0;
+        //store params
         int ro = stomps[row][0] - 1;
         int co = stomps[row][1] - 1;
         int le = stomps[row][2];
+        //loop through the squares with cows on them
         for (int y = ro; y < ro + 3 && y < r; y++){
           for (int x = co; x < co + 3 && x < c; x++){
+            //if greater than current max, set max
             if (lake[y][x] - le > max){
               max = lake[y][x] - le;
             }
@@ -42,7 +52,9 @@ public class USACO{
         }
         for (int y = ro; y < ro + 3 && y < r; y++){
           for (int x = co; x < co + 3 && x < c; x++){
+            // if the cows will reach square's level
             if (lake[y][x] > max){
+              //set the square to the new max el
               lake[y][x] = max;
             }
           }
@@ -55,14 +67,17 @@ public class USACO{
       int depths = 0;
       for (int row = 0; row < r; row++){
         for (int col = 0; col < c; col++){
+          //not possible for depth to be negative
           if (e - lake[row][col] > 0){
+            //add to total
             depths += (e - lake[row][col]);
           }
         }
       }
-
+      //return volume
       return depths * 72 * 72;
     }
+    //if no file
     catch(FileNotFoundException e){}
     return 0;
   }
@@ -84,5 +99,6 @@ public class USACO{
 
   public static void main(String[] args) {
     System.out.println(USACO.bronze("makelake.2.in") + " = 102762432");
+    System.out.println(USACO.bronze("makelake.4.in") + " = 753121152");
   }
 }
