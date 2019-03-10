192a9346ef3aac52da230614e8f46d696140683a
Sophie Nichol <snichol00@stuy.edu>
2019-03-10 17:32:42 -0400

    made separate driver

---
+++ Driver.java
@@ -0,0 +1,11 @@
+public class Driver{
+  public static void main(String[] args) {
+    System.out.println(USACO.bronze("makelake.2.in") + " = 102762432");
+    System.out.println(USACO.bronze("makelake.4.in") + " = 753121152");
+    System.out.println(USACO.silver("ctravel.1.in") + " = 1");
+    System.out.println(USACO.silver("ctravel.2.in") + " = 74");
+    System.out.println(USACO.silver("ctravel.3.in") + " = 6435");
+    System.out.println(USACO.silver("ctravel.4.in") + " = 339246");
+    System.out.println(USACO.silver("ctravel.5.in") + " = 0");
+  }
+}
